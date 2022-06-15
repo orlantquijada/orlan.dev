b882a6326baf870f92e037bfe67ec9a189a61686
Orlan Quijada <orlantquijada@gmail.com>
2022-06-15 23:59:19 +0800

    feat: fonts + ssr styling

--- apps/portfolio/pages/_document.tsx
+++ apps/portfolio/pages/_document.tsx
@@ -1,11 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from 'ui'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
           rel="stylesheet"
         />
       </Head>
