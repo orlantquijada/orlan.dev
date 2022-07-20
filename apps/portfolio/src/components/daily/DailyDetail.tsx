6483fc95d93261ae29c2ccd1a09d6e2c54b6e87d
Orlan Quijada <orlantquijada@gmail.com>
2022-07-20 12:46:06 +0800

    feat: daily entry detail ui

---
+++ apps/portfolio/src/components/daily/DailyDetail.tsx
@@ -0,0 +1,238 @@
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import { type Daily } from 'contentlayer/generated'
+import { Box, css, Flex, styled, Text, textStyles } from 'ui'
+import { useEffect, useState, type ComponentProps } from 'react'
+import { format } from 'date-fns'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
+import { motion } from 'framer-motion'
+
+interface Props {
+  daily: Daily
+}
+
+export default function DailyDetail({ daily }: Props) {
+  const Title = useMDXComponent(daily.title.code)
+  const Body = useMDXComponent(daily.body.code)
+  const Quote = useMDXComponent(daily.quote.code)
+
+  const show = useShowBackButton()
+
+  const dateFormat = format(new Date(`${daily.month} ${daily.day}`), 'LLLL do')
+
+  return (
+    <Wrapper>
+      <header>
+        <HeaderCover />
+        <HeaderContentWrapper>
+          <HeaderContent>
+            <Title components={headerTitleComponents} />
+            <Text css={{ flexShrink: 0 }}>{dateFormat}</Text>
+          </HeaderContent>
+        </HeaderContentWrapper>
+      </header>
+      <Main>
+        <Flex
+          direction="column"
+          align="center"
+          css={{ alignSelf: 'center', mb: '$8' }}
+        >
+          <Text>{dateFormat}</Text>
+          <Title components={titleComponents} />
+        </Flex>
+        <Quote components={quoteComponents} />
+        <Box css={{ alignSelf: 'flex-end', mt: '$2' }}>
+          — {daily.author}, <em>{daily.book}</em>, {daily.section}
+        </Box>
+        <Box as="article" css={{ mt: '$10' }}>
+          <Body components={bodyComponents} />
+        </Box>
+      </Main>
+      <Footer>
+        <FooterIconButton
+          initial={{ opacity: 0 }}
+          animate={{ opacity: show ? 1 : 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <ArrowLeftIcon className={footerIconStyles()} />
+        </FooterIconButton>
+      </Footer>
+    </Wrapper>
+  )
+}
+
+function useShowBackButton() {
+  const [showBackButton, setShowBackButton] = useState(false)
+
+  useEffect(() => {
+    const handleScrollToggle = () => {
+      const OFFSET = 80
+      if (window.scrollY > OFFSET && !showBackButton) setShowBackButton(true)
+      else if (window.scrollY < OFFSET && showBackButton)
+        setShowBackButton(false)
+    }
+    window.addEventListener('scroll', handleScrollToggle)
+
+    return () => window.removeEventListener('scroll', handleScrollToggle)
+  }, [showBackButton])
+
+  return showBackButton
+}
+
+const Wrapper = styled('div', {
+  '--contentMaxWidth': '650px',
+  '--contentPX': '$sizes$4',
+  '--headerHeight': 'min(15vw, $sizes$28)',
+  '--headerLayer': 10,
+  '--iconButtonSize': '4rem',
+
+  fontFamily: '"EB Garamond", serif',
+  color: '$textColor',
+})
+const Main = styled('main', {
+  maxWidth: 'var(--contentMaxWidth)',
+  mx: 'auto',
+  pt: 'var(--headerHeight)',
+  pb: 'calc($sizes$10 + var(--iconButtonSize))',
+  px: 'var(--contentPX)',
+
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+
+const HeaderCover = styled('div', {
+  background: '$bg',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  height: 'var(--headerHeight)',
+  zIndex: 'calc(var(--headerLayer) + 1)',
+})
+const HeaderContentWrapper = styled('div', {
+  zIndex: 'var(--headerLayer)',
+  borderBottom: '1px solid $olive6',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: '$bg',
+  height: '3rem',
+  display: 'flex',
+  alignItems: 'center',
+})
+const HeaderContent = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: '$4',
+
+  maxWidth: 'var(--contentMaxWidth)',
+  width: '100%',
+  mx: 'auto',
+  px: 'var(--contentPX)',
+})
+const HeaderTitleHeading = styled('h1', textStyles, {
+  color: '$textColor',
+  fontWeight: '$regular',
+  fontFamily: '"EB Garamond", serif',
+  display: 'block',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+})
+const headerTitleComponents = {
+  // override <p> tags to render as <h1> to avoid adding a necessary <h1> tag in the
+  // content files `title` frontmatter since `title`'s type is markdown to enable italics
+  p: (props: ComponentProps<typeof TitleHeading>) => (
+    <HeaderTitleHeading {...props} size="base" />
+  ),
+}
+
+const Footer = styled('footer', {
+  display: 'flex',
+  justifyContent: 'space-between',
+
+  position: 'fixed',
+  bottom: '$4',
+  left: 0,
+  right: 0,
+
+  maxWidth:
+    'calc(var(--iconButtonSize) * 2 + var(--contentMaxWidth) + $sizes$6)',
+  mx: 'auto',
+  px: '$4',
+
+  '@tab': {
+    bottom: '$10',
+  },
+})
+const FooterIconButton = styled(motion.button, {
+  cursor: 'pointer',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: 'none',
+  borderRadius: '$lg',
+  size: 'var(--iconButtonSize)',
+  backgroundColor: '$olive4',
+  transition: 'background-color 150ms ease',
+  '-webkit-tap-highlight-color': 'transparent',
+
+  '&:hover': { backgroundColor: '$olive5' },
+  '&:active': { backgroundColor: '$olive6' },
+
+  '@tab': {
+    opacity: '1 !important',
+  },
+})
+const footerIconStyles = css({
+  color: '$textColor',
+  size: '1.25rem',
+  fontWeight: '$medium',
+})
+
+const TitleHeading = styled('h1', textStyles, {
+  textAlign: 'center',
+})
+
+const titleComponents = {
+  // override <p> tags to render as <h1> to avoid adding a necessary <h1> tag in the
+  // content files `title` frontmatter since `title`'s type is markdown to enable italics
+  p: (props: ComponentProps<typeof TitleHeading>) => (
+    <TitleHeading {...props} size="2xl" />
+  ),
+}
+
+const QuoteParagraph = styled('p', textStyles, {
+  '&::before': {
+    content: 'open-quote',
+  },
+  '&::after': {
+    content: 'close-quote',
+  },
+})
+
+const quoteComponents = {
+  p: (props: ComponentProps<typeof QuoteParagraph>) => (
+    <QuoteParagraph {...props} size="xl" />
+  ),
+}
+
+const BodyParagraph = styled('p', textStyles, {
+  '&:not(:first-of-type)': {
+    textIndent: '2em',
+  },
+
+  '&:first-of-type::first-letter': {
+    float: 'left',
+    lineHeight: '85%',
+    width: '.7em',
+    fontSize: '325%',
+  },
+})
+
+const bodyComponents = {
+  p: (props: ComponentProps<typeof BodyParagraph>) => (
+    <BodyParagraph {...props} size="xl" />
+  ),
+}
