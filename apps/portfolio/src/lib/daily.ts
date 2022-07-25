3bc6ee1299049afc92dde4011072772b934e9e71
Orlan Quijada <orlantquijada@gmail.com>
2022-07-25 11:40:33 +0800

    finally fixed timezone issue
    * chore: july 25 daily entry
    
    * attempt #2
    
    * check
    
    * type fix
    
    * cleanup

--- apps/portfolio/src/lib/daily.ts
+++ apps/portfolio/src/lib/daily.ts
@@ -1,5 +1,7 @@
 import { allDailies, Daily } from 'contentlayer/generated'
-import { Month } from './contentlayer'
+import { parse } from 'date-fns'
+import { formatInTimeZone } from 'date-fns-tz'
+import { Month, Months } from './contentlayer'
 import { FilterFalseProps, KeysFlag, pickProps } from './utils'
 
 type FilterKeys = Extract<keyof Daily, 'author' | 'day' | 'month'>
@@ -38,4 +40,21 @@ export function getDailies<
   }
 
   return dailies as unknown as Result[]
+}
+
+export function getDailyToday(timezone = 'Asia/Manila') {
+  // correctly display `Daily` today based on timezone if given
+  // ^ above is necessary bec `getServerSideProps` uses UTC by default
+  const format = 'yyyy-MM-dd HH:mm:ss'
+  const today = parse(
+    formatInTimeZone(new Date(), timezone, format),
+    format,
+    new Date()
+  )
+
+  const dailyToday = getDailies({
+    filter: { month: Months[today.getMonth()], day: today.getDate() },
+  })
+
+  if (dailyToday.length) return dailyToday[0]
 }
