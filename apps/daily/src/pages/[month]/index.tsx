7482e6902404bee014bebfa7b26ce5eb48edf029
Orlan Quijada <orlantquijada@gmail.com>
2022-08-01 17:33:46 +0800

    wip: handled calendar month state

--- apps/daily/src/pages/[month]/index.tsx
+++ apps/daily/src/pages/[month]/index.tsx
@@ -8,20 +8,32 @@ import { allDailies, type Daily } from 'contentlayer/generated'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
 import { Months, type Month } from 'src/lib/contentlayer'
+import { css } from '@stitches.config'
+import Calendar from '@/components/Calendar'
+import { useRouter } from 'next/router'
 
 export default function MonthsNavPage({
   data,
-  month,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { query } = useRouter()
+  const currentMonth = capitalize(query.month as Lowercase<Month>)
+
   return (
-    <div>
-      {month}
+    <div className={main()}>
+      {currentMonth}
       {data.map((data) => (
         <Card data={data} key={data._id} />
       ))}
+
+      <Calendar month={currentMonth} />
     </div>
   )
 }
+
+const main = css({
+  maxWidth: '650px',
+  mx: 'auto',
+})
 
 function Card({ data: { title, url } }: { data: Data }) {
   const Title = useMDXComponent(title.code)
@@ -50,17 +62,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 type Data = Pick<Daily, '_id' | 'title' | 'url'>
 
 export const getStaticProps: GetStaticProps<
-  { month: Lowercase<Month>; data: Data[] },
+  { data: Data[] },
   { month: Lowercase<Month> }
 > = async ({ params }) => {
   if (!params) return { notFound: true }
 
   return {
     props: {
-      month: params.month,
       data: allDailies
         .filter(({ month }) => month.toLowerCase() === params.month)
         .map(({ _id, title, url }) => ({ _id, title, url })),
     },
   }
+}
+
+function capitalize<T extends string>(str: T) {
+  return `${str[0].toUpperCase()}${str.slice(1)}` as Capitalize<T>
 }
