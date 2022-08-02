24824bd9f00418472fe40bf1a3667bc9cf76e504
Orlan Quijada <orlantquijada@gmail.com>
2022-08-02 14:48:08 +0800

    feat: supported field filtering

--- apps/daily/src/pages/api/dailies.ts
+++ apps/daily/src/pages/api/dailies.ts
@@ -3,8 +3,17 @@ import { type Month } from 'src/lib/contentlayer'
 import { getDailies } from 'src/lib/api'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  const { month } = _req.query
-  const dailies = getDailies({ filter: { month: month as Month | undefined } })
+  const { month, fields } = _req.query
+
+  const select: Record<string, boolean> = {}
+  if (fields && typeof fields === 'string') {
+    for (const key of fields.split(',')) select[key] = true
+  }
+
+  const dailies = getDailies({
+    filter: { month: month as Month | undefined },
+    select,
+  })
 
   res.status(200).json(dailies)
 }
