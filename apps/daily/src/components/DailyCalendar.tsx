d436668fd7cb02acac2b7d8778fc36fd4627593c
Orlan Quijada <orlantquijada@gmail.com>
2022-08-03 14:49:03 +0800

    refactor: used custom Calendar component

--- apps/daily/src/components/DailyCalendar.tsx
+++ apps/daily/src/components/DailyCalendar.tsx
@@ -1,7 +1,10 @@
+import { format } from 'date-fns'
+import { useRouter } from 'next/router'
+
 import { getNextMonth, getPreviousMonth } from '@/lib/api'
 import { Month, MonthSubjectsMap } from '@/lib/contentlayer'
-import { useRouter } from 'next/router'
-import { Text } from './Text'
+import { Text } from '@/components/Text'
+import * as Calendar from '@/components/Calendar'
 
 export default function DailyCalendar({ month }: { month: Month }) {
   const router = useRouter()
@@ -18,36 +21,63 @@ export default function DailyCalendar({ month }: { month: Month }) {
   return (
     // root
     <div>
-      <header>
-        {/* title */}
-        <h1>{month}</h1>
+      <Calendar.Root>
+        <header>
+          {/* title */}
+          <h1>{month}</h1>
 
-        <h2>{MonthSubjectsMap[month]}</h2>
+          <h2>{MonthSubjectsMap[month]}</h2>
 
+          <div>
+            <Calendar.PreviousMonthButton onClick={() => handleRoute('prev')}>
+              left
+            </Calendar.PreviousMonthButton>
+            <Calendar.PreviousMonthButton onClick={() => handleRoute('next')}>
+              right
+            </Calendar.PreviousMonthButton>
+          </div>
+        </header>
+
+        {/* calendar body */}
         <div>
-          <button onClick={() => handleRoute('prev')}>left</button>
-          <button onClick={() => handleRoute('next')}>right</button>
-        </div>
-      </header>
+          {/* weekdays container */}
+          <div
+            style={{ display: 'grid', gridTemplateColumns: 'repeat(7, 1fr)' }}
+          >
+            <Text>Sun</Text>
+            <Text>Mon</Text>
+            <Text>Tue</Text>
+            <Text>Wed</Text>
+            <Text>Thu</Text>
+            <Text>Fri</Text>
+            <Text>Sat</Text>
+          </div>
 
-      {/* calendar body */}
-      <div>
-        {/* weekdays container */}
-        <div>
-          <Text>Sun</Text>
-          <Text>Mon</Text>
-          <Text>Tue</Text>
-          <Text>Wed</Text>
-          <Text>Thu</Text>
-          <Text>Fri</Text>
-          <Text>Sat</Text>
+          <Calendar.Days>
+            {(days) => (
+              <div
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(7, 1fr)',
+                }}
+              >
+                {days.map((day, index) => (
+                  <button
+                    key={day.toString()}
+                    style={
+                      index === 0
+                        ? { gridColumnStart: day.getDay() + 1 }
+                        : undefined
+                    }
+                  >
+                    {format(day, 'MMM d, y')}
+                  </button>
+                ))}
+              </div>
+            )}
+          </Calendar.Days>
         </div>
-
-        {/* days table */}
-        <div>
-          <button>1</button>
-        </div>
-      </div>
+      </Calendar.Root>
     </div>
   )
 }
