b321f65aedb4e873b82027def3fa963be5a1d41f
Orlan Quijada <orlantquijada@gmail.com>
2022-09-11 23:18:50 +0800

    chore: vitest config

---
+++ apps/daily/vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'contentlayer/generated': fileURLToPath(
+        new URL('./.contentlayer/generated', import.meta.url)
+      ),
+    },
+  },
+})
