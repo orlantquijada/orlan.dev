8fe201ca89b48b757ce93b70879285cfaa1af785
Orlan Quijada <orlantquijada@gmail.com>
2022-09-26 16:03:58 +0800

    fix: incorrect url on prod

--- apps/daily/src/components/MetaTags.tsx
+++ apps/daily/src/components/MetaTags.tsx
@@ -9,7 +9,11 @@ type Props = {
 
 export default function MetaTags(props: Props) {
   const { description, title, image } = props
-  const url = `${process.env.VERCEL_URL || 'localhost:3000'}${props.url}`
+  const url = `${
+    process.env.NODE_ENV === 'production'
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : 'http://localhost:3000'
+  }${props.url}`
 
   return (
     <Head>
