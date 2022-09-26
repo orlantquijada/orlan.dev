fa7597fa2843fe619a81419a4a4d3fcdc30c29ba
Orlan Quijada <orlantquijada@gmail.com>
2022-09-26 15:32:48 +0800

    feat: custom meta tags comp

---
+++ apps/daily/src/components/MetaTags.tsx
@@ -0,0 +1,36 @@
+type Props = {
+  title: string
+  description: string
+  url: string
+  image: string
+}
+
+export default function MetaTags(props: Props) {
+  const { description, title, image } = props
+  const url = `${process.env.VERCEL_URL || 'localhost:3000'}${props.url}`
+
+  return (
+    <>
+      {/* Primary Meta Tags  */}
+      <title>Meta Tags — Preview, Edit and Generate</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta property="image" content={image} />
+      <meta itemProp="image" content={image} />
+
+      {/* Open Graph / Facebook  */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter  */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </>
+  )
+}
