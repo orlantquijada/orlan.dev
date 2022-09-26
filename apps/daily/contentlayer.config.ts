aa6e799b42a6c54b206383c97268e34da2af3d85
Orlan Quijada <orlantquijada@gmail.com>
2022-09-26 13:47:13 +0800

    chore: september 30 daily entry

--- apps/daily/contentlayer.config.ts
+++ apps/daily/contentlayer.config.ts
@@ -19,6 +19,7 @@ export const Daily = defineDocumentType(() => ({
         'Epictetus',
         'Musonius Rufus',
         'Diogenes Laertius',
+        'Zeno',
       ],
       required: true,
     },
