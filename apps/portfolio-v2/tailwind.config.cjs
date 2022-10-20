e1474213ae3e6f7cfde9c3b971977e8134459572
Orlan Quijada <orlantquijada@gmail.com>
2022-10-20 16:35:02 +0800

    chore: deployment fout test + tailwind integ

--- apps/portfolio-v2/tailwind.config.cjs
+++ apps/portfolio-v2/tailwind.config.cjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require('tailwindcss/defaultTheme')
 
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
