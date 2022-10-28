2ec957456abb90a356e15bb9e21d5049b2fa9551
Orlan Quijada <orlantquijada@gmail.com>
2022-10-28 16:18:19 +0800

    chore(deps): added astro-compress + config

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 
+import compress from 'astro-compress'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
 })
