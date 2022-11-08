091b917f6986b93c9be0ae571974633a3547d8ec
Orlan Quijada <orlantquijada@gmail.com>
2022-11-09 00:11:08 +0800

    feat(wip): theme toggle script

--- apps/portfolio-v2/tailwind.config.cjs
+++ apps/portfolio-v2/tailwind.config.cjs
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -13,5 +13,6 @@ module.exports = {
       },
     },
   },
+  darkMode: 'class',
   plugins: [],
 }
