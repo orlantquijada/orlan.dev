29a0cbcdae48b92577f2d36fbe184d9e13b0a25d
Orlan Quijada <orlantquijada@gmail.com>
2022-11-10 15:45:38 +0800

    chore: vite plugin svgr config

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
+import svgr from 'vite-plugin-svgr'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), compress()],
   vite: {
+    plugins: [svgr()],
     ssr: {
       noExternal: ['@radix-ui/react-dialog'],
     },
