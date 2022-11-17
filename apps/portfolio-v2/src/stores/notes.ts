51f9f3d522e217a66f9d9561350db0a620c16fff
Orlan Quijada <orlantquijada@gmail.com>
2022-11-17 23:39:25 +0800

    feat: notes store

---
+++ apps/portfolio-v2/src/stores/notes.ts
@@ -0,0 +1,14 @@
+import { atom, action } from 'nanostores'
+
+export const selectedTags = atom<string[]>([])
+
+export const addTag = action(selectedTags, 'addTag', (store, tag) =>
+  store.set([...store.get(), tag])
+)
+
+export const removeTag = action(
+  selectedTags,
+  'removeTag',
+  (store, tagToRemove) =>
+    store.set(store.get().filter((tag) => tagToRemove !== tag))
+)
