5cc66fa14b070e2cd1ed12a430d64a741341c772
Orlan Quijada <orlantquijada@gmail.com>
2022-11-19 05:09:21 +0800

    feat: filter note card by tag animation

---
+++ apps/portfolio-v2/src/components/NoteCard/NoteCard.tsx
@@ -0,0 +1,46 @@
+import { motion } from 'framer-motion'
+import { noteCardStyles } from './styles'
+
+export type NoteCardProps = {
+  title: string
+  description?: string | undefined
+  href: string
+}
+
+export default function NoteCard(props: NoteCardProps) {
+  const { title, description, href } = props
+
+  return (
+    <a href={href} className={noteCardStyles()}>
+      <h3 className="font-medium">{title}</h3>
+      {description ? (
+        <p className="text-sm text-gray10 dark:text-gray11">{description}</p>
+      ) : null}
+    </a>
+  )
+}
+
+export type MotionNoteCardProps = NoteCardProps & {
+  selected: boolean
+  isSelecting: boolean
+}
+
+// ! temp solution cause i dont want a motion component wrapper div
+// e.g. <motion.div><NoteCard/></motion.div>
+export function MotionNoteCard(props: MotionNoteCardProps) {
+  const { title, description, href, selected, isSelecting } = props
+
+  return (
+    <motion.a
+      layoutId={title}
+      animate={isSelecting ? { opacity: !selected ? 0.3 : 1 } : {}}
+      href={href}
+      className={noteCardStyles()}
+    >
+      <h3 className="font-medium">{title}</h3>
+      {description ? (
+        <p className="text-sm text-gray10 dark:text-gray11">{description}</p>
+      ) : null}
+    </motion.a>
+  )
+}
