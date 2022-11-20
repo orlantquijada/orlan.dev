3c0bb2b44e10745b238146ba0ba36c8222c62b9c
Orlan Quijada <orlantquijada@gmail.com>
2022-11-21 06:14:08 +0800

    chore: mdx integration config + settings

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
-
 import compress from 'astro-compress'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import svgr from 'vite-plugin-svgr'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), compress()],
+  integrations: [tailwind(), react(), mdx(), compress()],
   vite: {
     plugins: [svgr()],
     ssr: {
