15907c632c4e89b18d60eff5be3e37d586e56911
Orlan Quijada <orlantquijada@gmail.com>
2022-11-27 11:48:20 +0800

    feat(wip): filter by tag

--- apps/portfolio-v2/src/lib/notes.ts
+++ apps/portfolio-v2/src/lib/notes.ts
@@ -9,3 +9,30 @@ export function getNoteFilename(file: string) {
   const split = file.split('/')
   return split[split.length - 1]?.split('.')[0]
 }
+
+export type TagMap = Map<string, Set<string>>
+
+// ! TO FIX
+export function buildTagMap(...noteTags: string[][]): TagMap {
+  const categoryGraph: TagMap = new Map()
+
+  for (const tags of noteTags) {
+    for (const tag of tags) {
+      const _tag = categoryGraph.get(tag)
+
+      // build nodes
+      if (!_tag) categoryGraph.set(tag, new Set(tags.filter((t) => t !== tag)))
+      else {
+        for (const t of tags) {
+          if (t !== tag) _tag.add(t)
+        }
+      }
+    }
+  }
+
+  return categoryGraph
+}
+
+export function intersectionSet<T>(s1: Set<T>, s2: Set<T>): Set<T> {
+  return new Set([...s1].filter((x) => s2.has(x)))
+}
