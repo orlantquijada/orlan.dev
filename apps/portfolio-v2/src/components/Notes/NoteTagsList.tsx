b0854b869db72994c4c5a7a703b1a6f66fec71e9
Orlan Quijada <orlantquijada@gmail.com>
2022-12-01 14:12:40 +0800

    fix: finally fixed spotify animation
    todo: fix distorted border-radius

--- apps/portfolio-v2/src/components/Notes/NoteTagsList.tsx
+++ apps/portfolio-v2/src/components/Notes/NoteTagsList.tsx
@@ -1,6 +1,6 @@
-import { ComponentProps, forwardRef } from 'react'
+import { type ComponentProps, forwardRef } from 'react'
 import { useStore } from '@nanostores/react'
-import { AnimatePresence, LayoutGroup, motion, Variants } from 'framer-motion'
+import { AnimatePresence, motion, type Variants } from 'framer-motion'
 import { ReactComponent as Close } from '@/icons/cross.svg'
 
 import Chip from '../Chip/Chip'
@@ -14,16 +14,7 @@ const variants: Variants = {
     opacity: 0,
   },
   slideIn: {
-    // x: -32,
     x: -4,
-    marginLeft: -32,
-    justifyContent: 'flex-end',
-  },
-}
-
-const tagTextVariants: Variants = {
-  slideIn: {
-    paddingLeft: 28,
   },
 }
 
@@ -43,67 +34,69 @@ export default function NoteTagsList(props: Props) {
 
   return (
     <div className="flex flex-wrap gap-2 mt-6 sm:max-w-[85%] justify-start">
-      <LayoutGroup>
-        <AnimatePresence mode="popLayout">
-          {isSelecting ? (
-            <Chip asChild>
-              <motion.button
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                onClick={() => clearTags()}
-              >
-                <Close className={styles.icon} />
-              </motion.button>
-            </Chip>
+      <AnimatePresence mode="popLayout">
+        {isSelecting ? (
+          <Chip asChild>
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              onClick={() => clearTags()}
+            >
+              <Close className={styles.icon} />
+            </motion.button>
+          </Chip>
+        ) : (
+          <Chip color="primary" asChild>
+            <motion.span variants={variants} exit="fadeOut">
+              all
+            </motion.span>
+          </Chip>
+        )}
+
+        {_selectedTags.map((tag, index) =>
+          index === 0 ? (
+            <Tag
+              tag={tag}
+              variants={variants}
+              layoutId={tag}
+              key={tag}
+              style={{ zIndex: 5 - index }}
+            />
           ) : (
-            <Chip color="primary" asChild>
-              <motion.span variants={variants} exit="fadeOut">
-                all
-              </motion.span>
-            </Chip>
-          )}
+            <Tag
+              tag={tag}
+              layoutId={tag}
+              variants={variants}
+              animate="slideIn"
+              key={tag}
+              style={{
+                backgroundColor: 'var(--gray11)',
+                borderColor: 'var(--gray12)',
+                zIndex: 5 - index,
 
-          {_selectedTags.map((tag, index) =>
-            index === 0 ? (
+                paddingLeft: 36,
+                marginLeft: -32,
+                justifyContent: 'flex-end',
+              }}
+            />
+          )
+        )}
+
+        {!isSelecting
+          ? tags.map((tag) => (
               <Tag
                 tag={tag}
-                variants={variants}
+                initial={{ opacity: 1 }}
                 layoutId={tag}
                 key={tag}
-                style={{ zIndex: 5 - index }}
-              />
-            ) : (
-              <Tag
-                tag={tag}
-                layoutId={tag}
                 variants={variants}
-                animate="slideIn"
-                key={tag}
-                style={{
-                  backgroundColor: 'var(--gray11)',
-                  borderColor: 'var(--gray12)',
-                  zIndex: 5 - index,
-                }}
+                exit="fadeOut"
               />
-            )
-          )}
-
-          {!isSelecting
-            ? tags.map((tag) => (
-                <Tag
-                  tag={tag}
-                  initial={{ opacity: 1 }}
-                  layoutId={tag}
-                  key={tag}
-                  variants={variants}
-                  exit="fadeOut"
-                />
-              ))
-            : visibleTags.map((tag) => (
-                <Tag tag={tag} variants={variants} layoutId={tag} key={tag} />
-              ))}
-        </AnimatePresence>
-      </LayoutGroup>
+            ))
+          : visibleTags.map((tag) => (
+              <Tag tag={tag} variants={variants} layoutId={tag} key={tag} />
+            ))}
+      </AnimatePresence>
     </div>
   )
 }
@@ -130,7 +123,7 @@ const Tag = forwardRef<
           else addTag(tag)
         }}
       >
-        <motion.span variants={tagTextVariants}>{tag}</motion.span>
+        <motion.span layout>{tag}</motion.span>
       </motion.button>
     </Chip>
   )
