76555f7ac2482e07043acfe8c86aa47368710d93
Orlan Quijada <orlantquijada@gmail.com>
2022-12-04 16:19:04 +0800

    fix: redundant tailwind classes

--- apps/portfolio-v2/src/components/Chip/styles.ts
+++ apps/portfolio-v2/src/components/Chip/styles.ts
@@ -2,26 +2,24 @@ import { cva } from 'cva'
 
 export const chipStyles = cva(
   [
-    'border rounded-full grid place-items-center leading-none cursor-pointer transition-colors',
+    'grid cursor-pointer place-items-center rounded-full border leading-none transition-colors focus-within:outline-none',
   ],
   {
     variants: {
       size: {
-        sm: 'h-6 px-2 text-xs min-w-[2.5rem]',
-        md: 'h-7 px-3 text-sm min-w-[3rem]',
+        sm: 'h-6 min-w-[2.5rem] px-2 text-xs',
+        md: 'h-7 min-w-[3rem] px-3 text-sm',
         lg: 'h-8 px-4',
         responsive:
-          'h-6 px-2 text-xs min-w-[2.5rem] md:h-7 md:px-3 md:text-sm md:min-w-[3rem]',
+          'h-6 min-w-[2.5rem] px-2 text-xs md:h-7 md:min-w-[3rem] md:px-3 md:text-sm',
       },
       color: {
-        gray: 'border-gray6 hover:border-gray7 bg-gray1 hover:bg-gray3 text-gray11',
-        primary:
-          'border-gray12 bg-gray12 dark:bg-gray12 hover:bg-gray12 dark:hover:bg-gray11 text-gray1',
+        gray: 'border-gray6 bg-gray1 text-gray11 hover:border-gray7 hover:bg-gray3',
+        primary: 'border-gray12 bg-gray12 text-gray1 focus-visible:bg-grayA11',
       },
       transluscent: {
-        true: 'dark:border-grayA4 dark:bg-grayA3 dark:hover:bg-grayA3 dark:hover:border-grayA10 dark:text-gray12',
-        false:
-          'dark:border-gray3 dark:bg-gray3 dark:hover:bg-gray4 dark:text-gray11',
+        true: 'dark:border-grayA4 dark:hover:border-grayA10 dark:focus-within:border-grayA10',
+        false: 'dark:border-gray3',
       },
     },
     defaultVariants: {
@@ -31,9 +29,15 @@ export const chipStyles = cva(
     },
     compoundVariants: [
       {
-        color: 'primary',
+        color: 'gray',
         transluscent: true,
-        className: 'dark:!text-gray1 dark:hover:!bg-gray12',
+        className: 'dark:bg-grayA3 dark:text-gray12',
+      },
+      {
+        color: 'gray',
+        transluscent: false,
+        className:
+          'dark:bg-gray3 dark:focus-within:bg-gray4 dark:hover:bg-gray4',
       },
     ],
   }
