5ba9437c9c81c5f238697c7a8a893c04c2724df1
Orlan Quijada <orlantquijada@gmail.com>
2023-01-06 00:58:44 +0800

    feat(wip): heart animation on double click

--- apps/daily/src/components/LikeWrapper.tsx
+++ apps/daily/src/components/LikeWrapper.tsx
@@ -1,21 +1,33 @@
 import { styled } from '@stitches.config'
-import { ReactNode, useRef } from 'react'
+import { ReactNode, useRef, useState } from 'react'
+import Image from 'next/image'
+import Heart from '../../public/heart.png'
+import { motion } from 'framer-motion'
 
 const DELAY = 300
+const HEART_SIZE = 130
 
 export default function LikeWrapper({ children }: { children: ReactNode }) {
   const timer = useRef<NodeJS.Timer | null>(null)
 
+  const [open, setOpen] = useState<
+    false | { y: number; x: number; rotate: number; key: number }
+  >(false)
+
   return (
     <Container
-      onClick={(_) => {
+      onClick={(e) => {
         if (timer.current) {
           clearTimeout(timer.current)
           timer.current = null
 
           // do something
-          // console.log('double tapped')
-          // console.log({ y: e.clientY, x: e.clientX })
+          setOpen({
+            x: e.pageX,
+            y: e.pageY,
+            rotate: 0,
+            key: new Date().getTime(),
+          })
         } else {
           timer.current = setTimeout(() => {
             timer.current = null
@@ -24,11 +36,50 @@ export default function LikeWrapper({ children }: { children: ReactNode }) {
       }}
     >
       {children}
+
+      {open ? (
+        <Box
+          initial={{
+            y: '-50%',
+            x: '-50%',
+            top: open.y,
+            left: open.x,
+            position: 'absolute',
+            rotate: `${getRandomInt(-25, 25)}deg`,
+          }}
+          css={{ width: HEART_SIZE, height: HEART_SIZE }}
+          animate={{
+            scale: [1, 0.85, 1],
+            y: -120,
+            // opacity: 0.2,
+            opacity: 0,
+          }}
+          transition={{ scale: { duration: 0.2, delay: 0 }, delay: 0.3 }}
+          key={open.key}
+        >
+          <Image
+            src={Heart}
+            alt="Heart"
+            objectFit="fill"
+            width={HEART_SIZE}
+            height={HEART_SIZE}
+          />
+        </Box>
+      ) : null}
     </Container>
   )
 }
 
 const Container = styled('div', {
-  // position: 'absolute',
-  // inset: 0,
+  position: 'relative',
+  overflow: 'clip',
+  maxWidth: '100vw',
 })
+
+const Box = styled(motion.div, {})
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
