f4de9d091fed0c9cc2e60f5da632b6a5228f0490
Orlan Quijada <orlantquijada@gmail.com>
2023-05-23 13:16:01 +0800

    chore: remove boolean cast

--- apps/daily/src/lib/like.ts
+++ apps/daily/src/lib/like.ts
@@ -3,9 +3,9 @@ import { Daily } from 'contentlayer/generated'
 export const like = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
   localStorage.setItem(`${month}/${day}`, JSON.stringify(true))
 }
+export const removeLike = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
+  return localStorage.removeItem(`${month}/${day}`)
+}
 export const getIsLiked = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
   return Boolean(localStorage.getItem(`${month}/${day}`))
-}
-export const removeLike = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
-  return Boolean(localStorage.removeItem(`${month}/${day}`))
 }
