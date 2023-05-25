194f60e424001f565fec43110f438ac5d20e8255
Orlan Quijada <orlantquijada@gmail.com>
2023-05-25 21:23:58 +0800

    feat: api lib for likes

--- apps/daily/src/lib/like.ts
+++ apps/daily/src/lib/like.ts
@@ -1,11 +1,56 @@
-import { Daily } from 'contentlayer/generated'
+import { Daily, allDailies } from 'contentlayer/generated'
+import { Month, Months } from './contentlayer'
 
-export const like = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
-  localStorage.setItem(`${month}/${day}`, JSON.stringify(true))
+///////////////////////// util /////////////////////////
+
+type BetterTypedDaily = Daily & { month: Month }
+const DAILY_KEY = '__daily_/'
+const getKey = ({ day, month }: Pick<Daily, 'month' | 'day'>) =>
+  `${DAILY_KEY}${month}/${day}`
+const parseKey = <T extends string = `${typeof DAILY_KEY}${Month}/${string}`>(
+  key: T
+) => {
+  const [, month, _day] = key.split('/')
+
+  const day = parseInt(_day, 10)
+  if (Months.includes(month as Month) && !Number.isNaN(day)) {
+    return {
+      success: true,
+      data: { month: month as Month, day },
+    } as const
+  }
+
+  return {
+    success: false,
+  } as const
 }
-export const removeLike = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
-  return localStorage.removeItem(`${month}/${day}`)
+
+///////////////////////// api /////////////////////////
+
+export const like = (daily: Pick<Daily, 'month' | 'day'>) => {
+  localStorage.setItem(getKey(daily), JSON.stringify(true))
 }
-export const getIsLiked = ({ day, month }: Pick<Daily, 'month' | 'day'>) => {
-  return Boolean(localStorage.getItem(`${month}/${day}`))
+export const removeLike = (daily: Pick<Daily, 'month' | 'day'>) => {
+  return localStorage.removeItem(getKey(daily))
+}
+export const getIsLiked = (daily: Pick<Daily, 'month' | 'day'>) => {
+  return Boolean(localStorage.getItem(getKey(daily)))
+}
+export const getAllLiked = () => {
+  type Like = Pick<BetterTypedDaily, 'month' | 'day'>
+  const likes: Like[] = []
+
+  Object.keys(localStorage).forEach((key) => {
+    for (const month of Months) {
+      if (key.startsWith(`${DAILY_KEY}${month}`)) {
+        const { success, data } = parseKey(key)
+        if (success) likes.push(data)
+      }
+    }
+  })
+  return likes.map((like) =>
+    allDailies.find(
+      ({ month, day }) => month === like.month && day === like.day
+    )
+  ) as Daily[]
 }
