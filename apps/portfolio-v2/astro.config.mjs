7875d29e7c4977e1aeb43869c33129b8276bd444
Orlan Quijada <orlantquijada@gmail.com>
2023-08-08 22:02:31 +0800

    chore: ran prettier

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import svgr from 'vite-plugin-svgr';
-import mdx from '@astrojs/mdx';
-
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import svgr from 'vite-plugin-svgr'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
   vite: {
     plugins: [svgr()],
     ssr: {
-      noExternal: ["@radix-ui/*"]
-    }
-  }
-});
+      noExternal: ['@radix-ui/*'],
+    },
+  },
+})
