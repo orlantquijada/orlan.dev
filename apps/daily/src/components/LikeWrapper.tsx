e2071d5b94391c8991d0cad3a9402b846bf9d858
Orlan Quijada <orlantquijada@gmail.com>
2023-09-12 23:06:17 +0800

    feat(wip): use optimized heart asset

--- apps/daily/src/components/LikeWrapper.tsx
+++ apps/daily/src/components/LikeWrapper.tsx
@@ -1,13 +1,15 @@
-import { motion } from 'framer-motion'
-import Image from 'next/image'
 import { PropsWithChildren, useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 import { useDoubleClick } from '@/hooks/useDoubleClick'
 import { like } from '@/lib/like'
 import { styled } from '@stitches.config'
 import { Daily } from 'contentlayer/generated'
 
-import Heart from '../../public/heart.png'
+// import Heart from '../../public/heart.png'
+import Heart from '../../public/heart.webp'
 
 const HEART_SIZE = 130
 const SKEW_DEG = 30
@@ -33,70 +35,103 @@ export default function LikeWrapper({ children, month, day, onLike }: Props) {
       like({ month, day })
       onLike?.()
     },
-    { delay: 300 }
+    { delay: 300 },
   )
 
   return (
-    <Container
-      onClick={handler}
-      /* onClick={(e) => { */
-      /*   if (timer.current) { */
-      /*     clearTimeout(timer.current) */
-      /*     timer.current = null */
-      /**/
-      /*     // do something */
-      /*     setOpen({ */
-      /*       x: e.pageX, */
-      /*       y: e.pageY, */
-      /*       rotate: 0, */
-      /*       key: new Date().getTime(), */
-      /*     }) */
-      /*     like({ month, day }) */
-      /*     onLike?.() */
-      /*   } else { */
-      /*     timer.current = setTimeout(() => { */
-      /*       timer.current = null */
-      /*     }, DELAY) */
-      /*   } */
-      /* }} */
-    >
-      {children}
+    <>
+      <Head>
+        <link rel="preload" href="heart.webp" as="image" />
+      </Head>
+      <Container
+        onClick={handler}
+        /* onClick={(e) => { */
+        /*   if (timer.current) { */
+        /*     clearTimeout(timer.current) */
+        /*     timer.current = null */
+        /**/
+        /*     // do something */
+        /*     setOpen({ */
+        /*       x: e.pageX, */
+        /*       y: e.pageY, */
+        /*       rotate: 0, */
+        /*       key: new Date().getTime(), */
+        /*     }) */
+        /*     like({ month, day }) */
+        /*     onLike?.() */
+        /*   } else { */
+        /*     timer.current = setTimeout(() => { */
+        /*       timer.current = null */
+        /*     }, DELAY) */
+        /*   } */
+        /* }} */
+      >
+        {children}
 
-      {open ? (
-        <Box
-          initial={{
-            y: '-50%',
-            x: '-50%',
-            top: open.y,
-            left: open.x,
-            position: 'absolute',
-            rotate: `${getRandomInt(-SKEW_DEG, SKEW_DEG)}deg`,
-          }}
-          css={{ width: HEART_SIZE, height: HEART_SIZE, userSelect: 'none' }}
-          animate={{
-            scale: [1, 0.85, 1, 1, 1.3],
-            y: -120,
-            opacity: 0,
-          }}
-          transition={{
-            scale: { delay: 0, times: [0, 0.15, 0.3, 0.5, 1] },
-            delay: 0.5,
-          }}
-          onAnimationComplete={() => setOpen(false)}
-          key={open.key}
-        >
-          <Image
-            src={Heart}
-            alt="Heart"
-            objectFit="fill"
-            width={HEART_SIZE}
-            height={HEART_SIZE}
-          />
-        </Box>
-      ) : null}
-    </Container>
+        {/* TODO: preload heart png */}
+        {open ? (
+          <Box
+            initial={{
+              y: '-50%',
+              x: '-50%',
+              top: open.y,
+              left: open.x,
+              position: 'absolute',
+              rotate: `${getRandomInt(-SKEW_DEG, SKEW_DEG)}deg`,
+            }}
+            css={{ width: HEART_SIZE, height: HEART_SIZE, userSelect: 'none' }}
+            animate={{
+              scale: [1, 0.85, 1, 1, 1.3],
+              y: -120,
+              opacity: 0,
+            }}
+            transition={{
+              scale: { delay: 0, times: [0, 0.15, 0.3, 0.5, 1] },
+              delay: 0.5,
+            }}
+            onAnimationComplete={() => setOpen(false)}
+            key={open.key}
+          >
+            {/* <HeartImage /> */}
+            {/* <picture> */}
+            {/*   <img */}
+            {/*     src="heart.webp" */}
+            {/*     // src="heart.svg" */}
+            {/*     style={{ objectFit: 'fill' }} */}
+            {/*     height={HEART_SIZE} */}
+            {/*     width={HEART_SIZE} */}
+            {/*     alt="asd" */}
+            {/*   /> */}
+            {/* </picture> */}
+            <Image
+              // src="/heart.png"
+              src={Heart}
+              alt="Heart"
+              objectFit="fill"
+              width={HEART_SIZE}
+              height={HEART_SIZE}
+              priority
+            />
+          </Box>
+        ) : null}
+      </Container>
+    </>
   )
 }
+
+// function HeartImage() {
+//   return (
+//     <Image
+//       // src="/heart.png"
+//       src={Heart}
+//       alt="Heart"
+//       objectFit="fill"
+//       width={HEART_SIZE}
+//       height={HEART_SIZE}
+//       priority
+//     />
+//   )
+// }
 
 const Container = styled('div', {
   position: 'relative',
