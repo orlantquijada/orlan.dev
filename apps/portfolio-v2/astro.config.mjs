d04c41f7d756b9fc3cd5467adb30f658d3149c6e
Orlan Quijada <orlantquijada@gmail.com>
2023-09-13 15:49:01 +0800

    fix: scope style

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -8,6 +8,7 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx(), compress()],
+  scopedStyleStrategy: 'where',
   vite: {
     plugins: [svgr()],
     ssr: {
