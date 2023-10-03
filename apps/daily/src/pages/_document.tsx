7a66c3c5f57fe9392aee0517fb66061377393c58
Orlan Quijada <orlantquijada@gmail.com>
2023-10-03 15:22:25 +0800

    fix: safari status bar overflow

--- apps/daily/src/pages/_document.tsx
+++ apps/daily/src/pages/_document.tsx
@@ -15,7 +15,6 @@ export default function Document() {
         />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
       </Head>
       <body>
         <Main />
