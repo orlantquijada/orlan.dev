2f11f13225c3a55d5d474385f82fe250235d406a
Orlan Quijada <orlantquijada@gmail.com>
2023-12-15 22:44:42 +0800

    fix: build issues

--- apps/daily/contentlayer.config.ts
+++ apps/daily/contentlayer.config.ts
@@ -74,6 +74,8 @@ const source = makeSource({
   contentDirPath: 'src/data',
   documentTypes: [Daily],
   mdx: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore type issue
     remarkPlugins: [[remarkSmartypants]],
   },
 })
