c1034d524dcda5245bbcc84c2776257358e0c70f
Orlan Quijada <orlantquijada@gmail.com>
2023-12-18 23:46:25 +0800

    chore: removed stitches related config

--- apps/daily/src/pages/_document.tsx
+++ apps/daily/src/pages/_document.tsx
@@ -1,14 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { getCssText } from '@stitches.config'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
