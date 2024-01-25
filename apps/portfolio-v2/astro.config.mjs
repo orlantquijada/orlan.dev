7fcd56556fdc32473b1b8e713fdb0bfc47e49ed9
Orlan Quijada <orlantquijada@gmail.com>
2024-01-25 21:50:08 +0800

    feat: upgraded to astro 4.2

--- apps/portfolio-v2/astro.config.mjs
+++ apps/portfolio-v2/astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import svgr from 'vite-plugin-svgr'
 import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), compress()],
+  integrations: [tailwind(), react(), mdx(), icon(), compress()],
   scopedStyleStrategy: 'where',
   vite: {
     plugins: [svgr()],
