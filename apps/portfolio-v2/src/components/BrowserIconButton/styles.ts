543bdb56a6970fd84bfbfd4a6c88130cad8d189c
Orlan Quijada <orlantquijada@gmail.com>
2024-02-09 03:07:10 +0800

    fix: demo video not working on android

---
+++ apps/portfolio-v2/src/components/BrowserIconButton/styles.ts
@@ -0,0 +1,5 @@
+import { cva } from 'cva'
+
+export const browserIconButtonStyles = cva(
+  'grid aspect-square h-10 place-items-center rounded-full bg-grayA2 text-gray11 opacity-0 transition-all duration-300 hover:bg-grayA3 hover:duration-150 dark:bg-gray12 dark:text-gray9',
+)
