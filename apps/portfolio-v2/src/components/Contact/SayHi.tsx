4d95ffa63c09f99ef99d0f7ed352c836836c0ea0
Orlan Quijada <orlantquijada@gmail.com>
2025-11-17 14:59:03 +0800

    Update background and header

--- apps/portfolio-v2/src/components/Contact/SayHi.tsx
+++ apps/portfolio-v2/src/components/Contact/SayHi.tsx
@@ -5,6 +5,8 @@ export default function SayHi() {
 		<ContactDialog>
 			<button
 				className="-mx-2 h-8 rounded-full px-2 text-base underline decoration-1 decoration-gray9 underline-offset-4 transition-colors hover:bg-accent hover:text-selectionColor hover:no-underline"
+				data-animate
+				style={{ "--stagger": 3 }}
 				type="button"
 			>
 				Say hi
