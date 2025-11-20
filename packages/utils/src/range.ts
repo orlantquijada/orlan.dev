2258db6a6085f491c3cbf61de1337739aa85cdc7
Orlan Quijada <orlantquijada@gmail.com>
2025-11-21 00:35:13 +0800

    Added duck detail in spaceduck link hover

---
+++ packages/utils/src/range.ts
@@ -0,0 +1,14 @@
+export function range(start: number, end?: number, step = 1) {
+  const output: number[] = [];
+
+  if (typeof end === "undefined") {
+    end = start;
+    start = 0;
+  }
+
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+
+  return output;
+}
