026f9ba82b560f0aa1364ae53baf9f26a2e648f5
Orlan Quijada <orlantquijada@gmail.com>
2025-12-11 03:51:46 +0800

    Update deps for react2shell

--- apps/portfolio-v2/src/utils/constants.ts
+++ apps/portfolio-v2/src/utils/constants.ts
@@ -3,3 +3,5 @@ export const DEFAULT_DATE_FORMAT = "MMM d, y";
 
 export const LINKED_IN = "https://www.linkedin.com/in/orlanquijada/";
 export const GITHUB = "https://github.com/orlantquijada";
+export const SPACEDUCK_LINK =
+	"https://www.linkedin.com/company/spaceduckapp/posts/?feedView=all";
